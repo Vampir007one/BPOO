7e9b7db813c5fa664f550a1efe2cecc91a78402a
Vampir007one <you@example.com>
2021-06-14 13:58:49 +0400

    first commit

---
+++ abiturient.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- links -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!--  -->
+    <title>График приёмной комиссии</title>
+</head>
+<body>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 header__icons">
+                <a href=""><img src="assets/img/vkontakte.png" alt="pic" class="icons__icon"></a>
+                <a href=""><img src="assets/img/instagram.png" alt="pic" class="icons__icon"></a>            
+                <a href=""><img src="assets/img/twitter.png" alt="pic" class="icons__icon"></a>
+            </div>
+            <div class="col-md-4 header__logo">
+                <img src="assets/img/bpoo_logo.png" alt="pic" class="logo__logo">
+                <p>Базовая профессиональная <br> образовательная организация <br> Самарской области</p>
+            </div>
+            <div class="col-md-4 header__feedbackIcons">
+                <div class="feedbackIcons__icon">
+                    <img src="assets/img/email.png" alt="pic">
+                    <p>Напишите нам <br> tspk@edu.tgl.ru</p>
+                </div>
+                <div class="feedbackIcons__icon">
+                    <img src="assets/img/smartphone.png" alt="pic" class="icon__2icon">
+                    <p>Позвоните нам <br> 8 (8482) 24-10-25</p>
+                </div>
+            </div>
+            <div class="col-md-12 menu">
+                <ul>
+                    <li><a href="/index.php">Главная</a></li>
+                    <li><a href="" class="menu__activeLink">График работы приёмной комиссии 2021</a></li>
+                    <li><a href="">Специальности</a></li>
+                    <li><a href="">Информация о БПОО</a></li>
+                    <li><a href="">Новости</a></li>
+                    <li><a href="">Контакты</a></li>
+                    <li><a href="">Форум</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- main -->
+<main>
+<div class="container-fluid">
+        <div class="row">
+            <!-- titles -->
+            <div class="col-md-12 main__title">
+                <p class="title__main tittle__abiturient">График работы приёмной комиссии 2021</p>
+            </div>
+            <div class="col-md-12 main__blocks">
+                <!-- 1block -->
+                <div class="blocks__block">
+                    <p class="block__text">Приёмная комиссия: ул. Мурысева 84 <br> Телефон: 24-10-25 <br> Кабинет: 114 <br> E-mail: tspk@edu.tgl.ru <br> Понедельник – Четверг: 8:30 – 17:00 | обед 12:30 – 13:00 <br> Пятница 8:30 – 16:00 | обед 12:30-13:00</p>
+                </div>
+                <!-- 1block -->
+                <!-- 2block -->
+                <div class="blocks__block">
+                    <p class="block__text"><span>Документы необходимые при подаче заявления:</span> <br> - паспорт (копия) <br> - аттестат (оригинал и копия) <br> - фото 4 шт. 3х4, медицинская справка, медицинская комиссия</p>
+                </div>
+                <!-- 2block -->
+                <!-- 3block -->
+                <div class="blocks__block">
+                    <p class="block__text">Прием документов на заочную форму получения образования осуществляется <span>до 25 августа 2021 г.</span> 
+                    <br> 
+                    Прием документов на очную форму получения образования осуществляется <span>до 15 августа 2021 г.</span> 
+                    <br> На специальности физическая культура и адаптивная физическая культура <span> с 01 июня до 10 августа 2021 года</span></p>
+                </div>
+                <!-- 3block -->
+                <!-- 4block -->
+                <div class="blocks__block">
+                    <p class="block__text">
+                    <span>Заявление о приеме с приложением необходимых документов подаются:</span><br>
+                    1) лично в приемную комииссию;<br>
+                    2) через операторов почтовой связи;<br>
+                    3) в электронной форме: посредством электронной почты организации или электронной информационной системы организации.
+                    </p>
+                </div>
+                <!-- 4block -->
+                <!-- 5block -->
+                <div class="blocks__block">
+                    <p class="block__text">
+                        <a href="">
+                            Согласно поправкам, с 1 января 2017 года отсрочка при поступлении в колледжи и техникумы предоставляется независимо от достижения призывником определенного возраста. Отсрочка от армии по учебе предоставляется на весь период обучения.
+                            (поправки к 24 статье ФЗ «О воинской обязанности и военной службе» от 28.03.1998 N 53-ФЗ)
+                        </a>
+                    </p>
+                </div>
+                <!-- 5block -->
+            </div>
+            <div class="col-md-12 main__buttonWork">
+                <a href=""><button>Ознакомиться со специальностями</button></a>
+            </div>
+        </div>
+</div>
+</main>
+<!-- /main -->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 footer__icons">
+                <a href=""><img src="assets/img/vkontakte.png" alt="pic" class="icons__icon"></a>
+                <a href=""><img src="assets/img/instagram.png" alt="pic" class="icons__icon"></a>            
+                <a href=""><img src="assets/img/twitter.png" alt="pic" class="icons__icon"></a>
+            </div>
+            <div class="col-md-4 footer__logo">
+                <a href="" class="footer__link">
+                    <img src="assets/img/bpoo_logo.png" alt="logo" class="logo__footerLogo">
+                    <p class="logo__text">Базовая профессиональная <br> образовательная организация <br> Самарской области</p>
+                </a>
+            </div>
+            <div class="col-md-4 footer__feedback">
+                <button><a href="">Напишите нам</a></button>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+<!-- links -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<!--  -->
+</html>
---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- links -->
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!--  -->
+    <title>Базовая профессиональная организация</title>
+</head>
+<body>
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 header__icons">
+                <a href=""><img src="assets/img/vkontakte.png" alt="pic" class="icons__icon"></a>
+                <a href=""><img src="assets/img/instagram.png" alt="pic" class="icons__icon"></a>            
+                <a href=""><img src="assets/img/twitter.png" alt="pic" class="icons__icon"></a>
+            </div>
+            <div class="col-md-4 header__logo">
+                <img src="assets/img/bpoo_logo.png" alt="pic" class="logo__logo">
+                <p>Базовая профессиональная <br> образовательная организация <br> Самарской области</p>
+            </div>
+            <div class="col-md-4 header__feedbackIcons">
+                <div class="feedbackIcons__icon">
+                    <img src="assets/img/email.png" alt="pic">
+                    <p>Напишите нам <br> tspk@edu.tgl.ru</p>
+                </div>
+                <div class="feedbackIcons__icon">
+                    <img src="assets/img/smartphone.png" alt="pic" class="icon__2icon">
+                    <p>Позвоните нам <br> 8 (8482) 24-10-25</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<main>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- titles -->
+            <div class="col-md-12 main__title">
+                <p class="title__main">Добро пожаловать на сайт <br> <span>Базовой профессиональной образовательной огранизации Самарской области.</span></p>
+                <p class="title__subtitle">Базовая профессиональная образовательная организация (БПОО) - профессиональная образовательная организация, обеспечивающая поддержку функционирования региональной системы профессионального образования инвалидов и лиц с ОВЗ в Самарской области. БПОО осуществляет методическое сопровождение организации и реализации профессионального образования в профессиональных образовательных организациях Самарской области.</p>
+                <p class="tittle__whoAreYou">КТО ВЫ?</p>
+            </div>
+            <!--  -->
+            <div class="row main__roles">
+                <!-- role 1 -->
+                <div class="col-md-4 roles__role">
+                    <a href="" class="role__link">
+                        <img src="assets/img/student.png" alt="studImg" class="role__roleImg">
+                        <p class="role__roleName">Студент</p>
+                        <div class="role__line"></div>
+                        <p class="role__description">Будучи под ролью <br> "Студент" <br> у вас будет доступ к информации по трудоустройству и банку данных работодателей</p>
+                    </a>
+                </div>
+                <!-- /role 1 -->
+
+                <!-- role 2 -->
+                <div class="col-md-4 roles__role">
+                    <a href="abiturient.php" class="role__link">
+                        <img src="assets/img/abiturient.png" alt="studImg" class="role__roleImg">
+                        <p class="role__roleName">Абитуриент</p>
+                        <div class="role__line"></div>
+                        <p class="role__description">Будучи под ролью <br> "Абитуриент" <br> у вас будет доступ к информации по специальностям и графику приёмной комиссии</p>
+                    </a>
+                </div>
+                <!-- /role 2 -->
+
+                <!-- role 3 -->
+                <div class="col-md-4 roles__role">
+                    <a href="" class="role__link">
+                        <img src="assets/img/teacher.png" alt="studImg" class="role__roleImg">
+                        <p class="role__roleName">Преподаватель</p>
+                        <div class="role__line"></div>
+                        <p class="role__description">Будучи под ролью <br> "Преподаватель" <br> у вас будет доступ ко всем вкладкам и особенным возможностям сайта</p>
+                    </a>
+                </div>
+                <!-- /role 3 -->
+
+            </div>
+        </div>
+    </div>
+</main>
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 footer__icons">
+                <a href=""><img src="assets/img/vkontakte.png" alt="pic" class="icons__icon"></a>
+                <a href=""><img src="assets/img/instagram.png" alt="pic" class="icons__icon"></a>            
+                <a href=""><img src="assets/img/twitter.png" alt="pic" class="icons__icon"></a>
+            </div>
+            <div class="col-md-4 footer__logo">
+                <a href="" class="footer__link">
+                    <img src="assets/img/bpoo_logo.png" alt="logo" class="logo__footerLogo">
+                    <p class="logo__text">Базовая профессиональная <br> образовательная организация <br> Самарской области</p>
+                </a>
+            </div>
+            <div class="col-md-4 footer__feedback">
+                <button><a href="">Напишите нам</a></button>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+<!-- links -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<!--  -->
+</html>
